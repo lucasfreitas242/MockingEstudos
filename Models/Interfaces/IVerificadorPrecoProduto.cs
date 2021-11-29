0253860b599a9a6ccb02bb35b6bd2047c331abca
lucasfreitas242 <lucasfrittas@gmail.com>
2021-11-28 22:36:36 -0300

    Adicionar arquivos de projeto.

---
+++ Models/Interfaces/IVerificadorPrecoProduto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models.Interfaces
+{
+    public interface IVerificadorPrecoProduto
+    {
+        string VerificaPrecoProduto(Product p);
+    }
+}
---
+++ Models/VerificadorPrecoProduto.cs
@@ -0,0 +1,22 @@
+﻿using Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class VerificadorPrecoProduto : IVerificadorPrecoProduto
+
+    {
+        public string VerificaPrecoProduto(Product p)
+        {
+            if (p.Preco > 100)
+                return "Produto caro!";
+            else if (p.Preco <= 100 && p.Preco > 40)
+                return "Produto na média de Preço!";
+            else
+                return "Produto barato!";
+        }
+    }
+}
