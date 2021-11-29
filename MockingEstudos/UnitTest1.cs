12b5311e14529f4cd7c9a103e61414295d99a6f4
lucasfreitas242 <lucasfrittas@gmail.com>
2021-11-28 23:31:24 -0300

    Implementando o uso do Moq e realizando um teste comparativo referente as variações de Preço do produto!

--- MockingEstudos/UnitTest1.cs
+++ MockingEstudos/UnitTest1.cs
@@ -1,3 +1,6 @@
+using Models;
+using Models.Interfaces;
+using Moq;
 using System;
 using Xunit;
 
@@ -6,9 +9,45 @@ namespace MockingEstudos
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        [Trait ("Categoria", "Preço Correto")]
+        public void ValidaVerificadorPrecoProduto()
         {
+            //Arrange
+            Product ProdutoBarato = new Product()
+            {
+                Preco = 35
+            };
+            Mock<IVerificadorPrecoProduto> mock = new Mock<IVerificadorPrecoProduto>();
+            mock.Setup(m => m.VerificaPrecoProduto(ProdutoBarato)).Returns("Produto barato!");
+            VerificadorPrecoProduto verif = new VerificadorPrecoProduto();
 
+            //Act
+            var resultadoEsperado = mock.Object.VerificaPrecoProduto(ProdutoBarato);
+            var resultado = verif.VerificaPrecoProduto(ProdutoBarato);
+
+            //Assert
+            Assert.Equal(resultado, resultadoEsperado);
+        }
+
+        [Fact]
+        [Trait("Categoria", "Preço Incorreto")]
+        public void ValidaVerificadorPrecoProdutoFalse()
+        {
+            //Arrange
+            Product ProdutoBarato = new Product()
+            {
+                Preco = 55
+            };
+            Mock<IVerificadorPrecoProduto> mock = new Mock<IVerificadorPrecoProduto>();
+            mock.Setup(m => m.VerificaPrecoProduto(ProdutoBarato)).Returns("Produto barato!");
+            VerificadorPrecoProduto verif = new VerificadorPrecoProduto();
+
+            //Act
+            var resultadoEsperado = mock.Object.VerificaPrecoProduto(ProdutoBarato);
+            var resultado = verif.VerificaPrecoProduto(ProdutoBarato);
+
+            //Assert
+            Assert.Equal(resultado, resultadoEsperado);
         }
     }
 }
--- Models/Startup.cs
+++ Models/Startup.cs
@@ -28,10 +28,10 @@ namespace Models
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Models", Version = "v1" });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Models", Version = "v1" });
+            //});
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +40,8 @@ namespace Models
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Models v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Models v1"));
             }
 
             app.UseHttpsRedirection();
